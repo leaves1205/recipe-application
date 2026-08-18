00885f55ad0ac6d690fcb828c7639309f7a27b3f
Yunqi Cai <z5500822@ad.unsw.edu.au>
2026-08-18 18:54:57 +1000

    event handlers

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,9 +31,11 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886',
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
     );
     const data = await res.json();
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -160,3 +162,7 @@ const showRecipe = async function () {
   }
 };
 showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+
+window.addEventListener('hashchange', showRecipe);
+window.addEventListener('load', showRecipe);
